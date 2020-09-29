3b86dd0404f6bdf34c7f006a7b47d8423822f180
Alex Gordeev <gordeeffff@gmail.com>
2020-09-29 17:56:38 +0300

    feat: solve case

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 
 exports.min = function min (array) {
-  return 0;
+    let res;
+    if (array === undefined) {
+        res = 0;
+    }
+    else if(array.length === 0) {
+        res = 0;
+    }
+    else {
+        for(let i = 0; i < array.length; i++) {
+            res = array[0];
+            for(let i = 1; i < array.length; i++) {
+                if(array[i] < res) {
+                    res = array[i];
+                }
+            }
+        }
+    }
+    return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let res;
+    if (array === undefined) {
+        res = 0;
+    }
+    else if(array.length === 0) {
+        res = 0;
+    }
+    else {
+        res = array[0];
+        for(let i = 1; i < array.length; i++) {
+            if(array[i] > res) {
+                res = array[i];
+            }
+        }
+    }
+    return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let res;
+    if (array === undefined) {
+        res = 0;
+    }
+    else if(array.length === 0) {
+        res = 0;
+    }
+    else {
+        let total = 0;
+        for(let i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        res = total / array.length;
+    }
+    return res;
 }
